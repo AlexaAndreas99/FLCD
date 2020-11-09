cb2a7064c66d52ed5fabb5983fd03655d1df454f
Alexa Andreas <alexaandreas99@gmal.com>
2020-11-09 13:10:48 +0200

    lab4

---
+++ lab4/FiniteAutomata.py
@@ -0,0 +1,71 @@
+import re
+
+
+class FiniteAutomata:
+
+    def __init__(self):
+        self.states = []
+        self.input_symbols = []
+        self.initial_state = []
+        self.final_states = []
+        self.transitions = {}
+        # Transitions structure
+        # {A:{'1':'B', '0':'A'},
+        #  B:{'1':'B', '0':'C'},
+        #  C:{}}
+
+    def reader(self):
+        f = open("FA.in", "r")
+        row = 0
+        for x in f:
+            result = []
+            line = re.split(' |\n', x)
+            for i in line:
+                result.append(i)
+            while '' in result:
+                result.remove('')
+            self.add_to_fa(result, row)
+            row += 1
+
+        for i in self.final_states:
+            if i not in self.transitions.keys():
+                self.transitions[i] = {}
+
+    def add_to_fa(self, res, i):
+        if i == 0:
+            self.states = res
+        elif i == 1:
+            self.input_symbols = res
+        elif i == 2:
+            self.initial_state = res
+        elif i == 3:
+            self.final_states = res
+        else:
+            if res[0] not in self.transitions.keys():
+                self.transitions[res[0]] = {res[1]: res[3]}
+            else:
+                self.transitions[res[0]][res[1]] = res[3]
+
+    def accepts(self, s):
+        state = self.initial_state[0]
+        for c in s:
+            if c not in self.transitions[state]:
+                raise KeyError
+            state = self.transitions[state][c]
+
+        return state in self.final_states
+
+    def print_transitions(self):
+        print(self.transitions)
+
+    def print_states(self):
+        print(self.states)
+
+    def print_input_symbols(self):
+        print(self.input_symbols)
+
+    def print_initial_state(self):
+        print(self.initial_state)
+
+    def print_final_states(self):
+        print(self.final_states)
