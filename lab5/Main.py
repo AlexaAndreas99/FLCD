5355f18b14882bd175f8862858c7427d083e1438
Sergiu Apostu <sergiu.apostu25@gmail.com>
2020-11-23 19:54:03 +0200

    Add interface

---
+++ lab5/Main.py
@@ -0,0 +1,63 @@
+from Grammar import Grammar
+from Parser import Parser, ParseException
+
+
+def g1(g):
+    parser = Parser(g)
+    try:
+        parser.parse("a c b c")
+        # print(parser.parse("a c b a"))
+        parser.build_tree()
+    except ParseException:
+        print('Error')
+
+
+def g2(g):
+    parser = Parser(g)
+    try:
+        parser.parse("num y . listen ( y ) .")
+        # parser.parse("seq i from 1 to half with i = i + 1 : kick . end .")
+        parser.build_tree()
+    except ParseException:
+        print('Error')
+
+
+def menu():
+    s = ''
+    s += '0. Exit\n'
+    s += '1. Set of non-terminals\n'
+    s += '2. Set of terminals\n'
+    s += '3. Set of productions\n'
+    s += '4. Productions for a given non_terminal\n'
+    s += '5. Parse\n'
+    print(s)
+
+
+if __name__ == '__main__':
+    while True:
+        g = 'g2'
+        grammar = Grammar()
+        grammar.read_file(g + ".txt")
+
+        print('')
+        menu()
+
+        i = input('> ')
+        if i == '1':
+            print(grammar.non_terminals)
+        elif i == '2':
+            print(grammar.terminals)
+        elif i == '3':
+            print(grammar.productions)
+        elif i == '4':
+            non_terminal = input('Give a non terminal > ')
+            print(grammar.non_terminal_productions(non_terminal))
+        elif i == '5':
+            if g == 'g1':
+                g1(grammar)
+            elif g == 'g2':
+                g2(grammar)
+        elif i == '0':
+            break
+        else:
+            print('Command failed successfully')
